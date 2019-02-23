818da9d6c5aa94e226104f6dcf4a4eb7f708a5b5
Maksim Samuilionak <maximwow70@gmail.com>
2019-02-23 12:31:12 +0300

    Remove prefix.

--- frontend/andromeda/src/app/app.component.ts
+++ frontend/andromeda/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'an--app',
+  selector: 'app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
