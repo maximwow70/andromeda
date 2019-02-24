3889d0da0b0dea107751515dd70cc1b20762174e
Maksim Samuilionak <maximwow70@gmail.com>
2019-02-24 12:42:53 +0300

    [AN--HOME] Create home component.

---
+++ frontend/andromeda/src/app/an--home/an--home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'an--home',
+	templateUrl: './an--home.component.html',
+	styleUrls: ['./an--home.component.scss']
+})
+export class AnHomeComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
